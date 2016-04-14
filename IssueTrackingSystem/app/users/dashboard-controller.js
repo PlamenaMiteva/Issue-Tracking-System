8da9d9562ecf63cebf5979921f70db8aa6c76016
Plamena Miteva <plamenamiteva@abv.bg>
2016-04-14 17:54:11 +0300

    dashboard pagination added

--- IssueTrackingSystem/app/users/dashboard-controller.js
+++ IssueTrackingSystem/app/users/dashboard-controller.js
@@ -11,9 +11,51 @@ angular.module('issueTrackingSystem.users.board', [
     }])
     .controller('BoardCtrl', [
         '$scope',
+        '$location',
         'dashboard',
-        function ($scope, dashboard) {
-            dashboard.showUserDashboard().then(function (issues) {
-                $scope.issues = issues.data.Issues;                
-            });
+        function ($scope, $location, dashboard) {
+
+            $scope.pageNumber= 1;
+            $scope.pageSize = 3;
+            $scope.pageArray = [];            
+           
+            dashboard.showUserDashboard($scope.pageSize, $scope.pageNumber)
+               .then(function (issues) {
+                   $scope.issues = issues.data.Issues;
+                   $scope.pages = issues.data.TotalPages;
+                   for (var i = 1; i <= $scope.pages; i++) {
+                       $scope.pageArray.push(i);
+                   }                   
+               });
+
+            $scope.nextPage = function () {
+                if ($scope.pageNumber < $scope.pages) {
+                    $scope.pageNumber++;
+                    dashboard.showUserDashboard($scope.pageSize, $scope.pageNumber)
+                    .then(function (issues) {
+                        $scope.issues = issues.data.Issues;
+                        $scope.pages = issues.data.TotalPages;
+                    });
+                }
+            };
+
+            $scope.getCurrentPage = function (page) {                   
+                    dashboard.showUserDashboard($scope.pageSize, page)
+                    .then(function (issues) {
+                        console.log(page)
+                        $scope.issues = issues.data.Issues;
+                        $scope.pages = issues.data.TotalPages;
+                    });                
+            };
+
+            $scope.previousPage = function () {
+                if ($scope.pageNumber > 1) {
+                    $scope.pageNumber--;
+                    dashboard.showUserDashboard($scope.pageSize, $scope.pageNumber)
+                    .then(function (issues) {
+                        $scope.issues = issues.data.Issues;
+                        $scope.pages = issues.data.TotalPages;
+                    });
+                }
+            };
         }]);
--- IssueTrackingSystem/app/users/dashboard.js
+++ IssueTrackingSystem/app/users/dashboard.js
@@ -1,38 +1,27 @@
 angular.module('issueTrackingSystem.users.dashboard', [])
-        .factory('dashboard', [
+        .factory('dashboard', [            
             '$http',
             '$q',            
             'BASE_URL',
             'authentication',
-            function ($http, $q, BASE_URL, authentication) {               
+            function ($http, $q, BASE_URL, authentication) {
+               
+                    function showUserDashboard(pageSize, pageNumber) {
 
-                function showUserDashboard(pageSize, pageNumber) {
                     var deferred = $q.defer();                    
-                    pageSize = pageSize || 10;               
-                    pageNumber = pageNumber || 1;
-                    data = "Bearer " + localStorage['accessToken'];
                     
+                    data = "Bearer " + localStorage['accessToken'];                    
                 
                     $http.get(BASE_URL + 'Issues/me?pageSize=' + pageSize + '&pageNumber=' + pageNumber + '&orderBy=DueDate', { headers: { 'Authorization': data } })
-                            .then(function(result) {
-                                deferred.resolve(result);
-                                console.log(result);
-                            });
-                    
-                    //data = "Bearer " + localStorage['accessToken'];                    
-                    //$http.get(BASE_URL + 'projects', { headers: { 'Authorization': data } })
-                    //        .then(function (response) { 
-                    //        deferred.resolve(response);
-                    //    }, function (error) {
-
-                    //    });
+                            .then(function (result) {         
+                                deferred.resolve(result);                                
+                            });  
 
                     return deferred.promise;
                 }
-
                
                 return {
-                       showUserDashboard: showUserDashboard
+                    showUserDashboard: showUserDashboard   
                 }
             }]);
 
