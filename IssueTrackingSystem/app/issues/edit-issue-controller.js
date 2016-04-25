956da5cac6d830fb257e8bc60aaf46765a7aa1b8
Plamena Miteva <plamenamiteva@abv.bg>
2016-04-25 16:49:07 +0300

    calendar added to edit issue page

--- IssueTrackingSystem/app/issues/edit-issue-controller.js
+++ IssueTrackingSystem/app/issues/edit-issue-controller.js
@@ -24,6 +24,16 @@ angular.module('issueTrackingSystem.edit-issue', [
                     $scope.isLead = false;
                     $scope.isAssignee = false;
 
+                    identity.getCurrentUser()
+                   .then(function (user) {                       
+                       if (user.Id === issue.data.Author.Id) {
+                           $scope.isLead = true;
+                       }
+                       if (user.Id === issue.data.Assignee.Id) {
+                           $scope.isAssignee = true;
+                       }
+                   });
+
                     usersService.getAllUsers()
                         .then(function (response) {
                             $scope.users = response.data;
@@ -32,16 +42,95 @@ angular.module('issueTrackingSystem.edit-issue', [
                     project.getProjectById(issue.data.Project.Id)
                         .then(function (response) {
                             $scope.avilablePriorities = response.data.Priorities;                          
-                        });
-
-                    var currentUser = identity.requestUserProfile();
-                    if(currentUser.Id=== issue.data.Author.id){
-                        $scope.isLead = true;
-                    }
-                    if(currentUser.Id=== issue.data.Assignee.Id){
-                        $scope.isAssignee = true;
-                    }
+                        });                   
+                   
                 });
+
+            $scope.today = function () {
+                $scope.dt = new Date();
+            };
+            $scope.today();
+
+            $scope.clear = function () {
+                $scope.dt = null;
+            };
+
+            $scope.inlineOptions = {
+                customClass: getDayClass,
+                minDate: new Date(),
+                showWeeks: true
+            };
+
+            $scope.dateOptions = {
+                dateDisabled: disabled,
+                formatYear: 'yy',
+                maxDate: new Date(2020, 5, 22),
+                minDate: new Date(),
+                startingDay: 1
+            };
+
+            // Disable weekend selection
+            function disabled(data) {
+                var date = data.date,
+                    mode = data.mode;
+                return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+            }
+
+            $scope.toggleMin = function () {
+                $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+                $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+            };
+
+            $scope.toggleMin();
+
+            $scope.open1 = function () {
+                $scope.popup1.opened = true;
+            };
+
+            $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
+            $scope.altInputFormats = ['M!/d!/yyyy'];
+
+            $scope.popup1 = {
+                opened: false
+            };
+
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            var afterTomorrow = new Date();
+            afterTomorrow.setDate(tomorrow.getDate() + 1);
+            $scope.events = [
+              {
+                  date: tomorrow,
+                  status: 'full'
+              },
+              {
+                  date: afterTomorrow,
+                  status: 'partially'
+              }
+            ];
+
+            function getDayClass(data) {
+                var date = data.date, mode = data.mode;
+                if (mode === 'day') {
+                    var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                    for (var i = 0; i < $scope.events.length; i++) {
+                        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                        if (dayToCheck === currentDay) {
+                            return $scope.events[i].status;
+                        }
+                    }
+                }
+
+                return '';
+            }
+
+            $scope.setDueDate = function setDuedate(dt) {
+                $scope.DueDate = dt;
+                console.log($scope.DueDate);
+            }
 
             $scope.changeStatus = function (issueId, status) {
                 issue.changeStatus(issueId, status.Id)
@@ -53,6 +142,7 @@ angular.module('issueTrackingSystem.edit-issue', [
 
             $scope.editIssue = function editIssue (id, input, labels) {
                 input.Labels = labels;
+                input.DueDate = $scope.DueDate;
                 issue.editIssue(id, input)
                     .then(function (result) {
                         $location.path('/issues/' + id);
